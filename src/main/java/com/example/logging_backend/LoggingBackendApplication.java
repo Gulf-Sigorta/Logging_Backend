9b145e7e22f65656b2e493014fbf45d035d7688d
Caner Akduruk <cakduruk@gig.net.tr>
2025-07-10 08:57:15 +0300

    Proje yüklendi

---
+++ src/main/java/com/example/logging_backend/LoggingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.logging_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingBackendApplication.class, args);
+	}
+
+}
